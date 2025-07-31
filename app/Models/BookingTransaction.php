2b4fac7e92781f3bfe8267b3832050f4e2e4639b
Bagasbelajarhtml <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-07-31 07:40:25 +0700

    add relation and instal package filament

--- app/Models/BookingTransaction.php
+++ app/Models/BookingTransaction.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BookingTransaction extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'name',
+        'phone_number',
+        'booking_trx_id',
+        'is_paid',
+        'office_space_id',
+        'total_amount',
+        'duration',
+        'started_at',
+        'ended_at'
+    ];
+
+    public function generateUniqueTrxId()
+    {
+        $prefix = 'BRP0';
+        do {
+            $randomcode =   $prefix . mt_rand(1000, 9999);
+        } while (self::where('booking_trx_id', $randomcode)->exists());
+
+        return $randomcode;
+    }
+
+    public function office_space()
+    {
+        return $this->belongsTo(OfficeSpace::class, 'office_space_id');
+    }
 }
--- app/Models/OfficeBenefit.php
+++ app/Models/OfficeBenefit.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OfficeBenefit extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'name',
+        'office_space_id',
+    ];
+
+    public function office_space(){
+        return $this->belongsTo(OfficeSpace::class,'office_space_id');
+    }
 }
